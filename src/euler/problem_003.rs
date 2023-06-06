52d7efbc9df2404f5623d6ebb95953a10fcedc9d
James Carr <james.r.carr@gmail.com>
2023-06-06 06:04:27 -0500

    Refactor to use the primal crate to simplfiy solution

--- src/euler/problem_003.rs
+++ src/euler/problem_003.rs
@@ -1,7 +1,9 @@
-// 
+extern crate primal;
+
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // 
 // What is the largest prime factor of the number 600851475143 ?
+
 pub fn solve() {
     let result = largest_prime_factor(600851475143);
 
@@ -9,33 +11,14 @@ pub fn solve() {
 }
 
 pub fn largest_prime_factor(n: u64) -> u64 {
-    let mut factors = n;
-    let mut max_prime = 0;
+    // Create a sieve with an approximation of the needed size
+    let sieve = primal::Sieve::new((n as f64).sqrt() as usize + 1);
 
-    // Print the number of 2s that divide n
-    while factors % 2 == 0 {
-        max_prime = 2;
-        factors /= 2;
-    }
+    // Use the `factors` method of the sieve to get the prime factors of n
+    let factors = sieve.factor(n as usize).unwrap();
 
-    // n must be odd at this point so a skip of 2 can be used
-    let mut i = 3;
-    while i <= (factors as f64).sqrt() as u64 {
-        // while i divides n , print i and divide n
-        while factors % i == 0 {
-            max_prime = i;
-            factors /= i;
-        }
-        i += 2;
-    }
-
-    // This condition is to handle the case when n is a prime number
-    // greater than 2
-    if factors > 2 {
-        max_prime = factors;
-    }
-
-    max_prime
+    // Get the largest factor
+    factors.last().unwrap().0 as u64
 }
 
 #[cfg(test)]
